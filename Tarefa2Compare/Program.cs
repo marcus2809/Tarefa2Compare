a27a3e49bab4939ff42237a731aebb59478d001f
Marcus Vinícius <marcusvinicius.paz2001@gmail.com>
2023-01-27 14:09:04 -0300

    Adicionar arquivos de projeto.

---
+++ Tarefa2Compare/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var compare = new Compare();
+
+Console.WriteLine(compare.CompareEquals(1, 2));
+Console.WriteLine(compare.CompareEquals("a", "b"));
+Console.WriteLine(compare.CompareEquals(1d, 1.1d));
+
+
+class Compare
+{
+    public bool CompareEquals(string a, string b)
+    {
+        return (a == b);
+    }
+
+    public bool CompareEquals(int a, int b)
+    {
+        return (a == b);
+    }
+
+    public bool CompareEquals(double a, double b)
+    {
+        return (a == b);
+    }
+}
